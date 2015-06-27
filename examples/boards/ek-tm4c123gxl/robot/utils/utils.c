f7ae92f379bd7ec08f0bc5b1c0e2c5354ad287f3
Marcin <marcin.gozdziewski@gmail.com>
2015-06-27 17:38:41 +0200

    Utility functions file creation
    - added ZeroBuffer function

---
+++ examples/boards/ek-tm4c123gxl/robot/utils/utils.c
@@ -0,0 +1,14 @@
+//****************************
+// Utility functions
+// autor: Marcin Gozdziewski
+// ***************************
+
+void ZeroBuffer(void* buffer, int len)
+{
+	unsigned char* uchPtr = (unsigned char*) buffer;
+
+	for(int i = 0; i != len; i++)
+	{
+		*uchPtr = 0;
+	}
+}
---
+++ examples/boards/ek-tm4c123gxl/robot/utils/utils.h
@@ -0,0 +1,11 @@
+//****************************
+// Utility functions
+// autor: Marcin Gozdziewski
+// ***************************
+
+#ifndef UTILS_H
+#define UTILS_H
+
+void ZeroBuffer(void* buffer, int len);
+
+#endif // UTILS_H
